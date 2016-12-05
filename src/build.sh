a349c6aab2f4524e6f67ff3c847a377c49a2c9de
Janis Ozols <cppkonsultants@gmail.com>
2016-12-05 22:16:47 +0200

    Disabled 'make check' for now

--- src/build.sh
+++ src/build.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 export PATH=$PATH:/home/cppko/Android/Sdk/ndk-bundle/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin
-make all && make check
+make all
+# cppkonsultants: compilation doesn't work for make check currently,
+# commented out
+# && make check
