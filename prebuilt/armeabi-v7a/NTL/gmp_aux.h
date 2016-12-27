dc877332809e528566ded9690d24a359be6c559f
Janis Ozols <cppkonsultants@gmail.com>
2016-12-27 21:33:45 +0200

    Support for multiple multiple ABIs

---
+++ prebuilt/armeabi-v7a/NTL/gmp_aux.h
@@ -0,0 +1,2 @@
+WARNING: linker: ./gen_gmp_aux: unused DT entry: type 0x6ffffffe arg 0x324
+WARNING: linker: ./gen_gmp_aux: unused DT entry: type 0x6fffffff arg 0x1
--- src/build.sh
+++ src/build.sh
@@ -1,6 +1,39 @@
 #!/bin/sh
-export PATH=$PATH:/home/cppko/Android/Sdk/ndk-bundle/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin
-make all
+
+export NDK_DIR=$HOME/Android/Sdk/ndk-bundle
+ORIG_PATH=$PATH
+
+mkdir -p ../build
+
+# Rest of ABI builds commented out because there must be one device
+# connected at the time of build.
+# ANDROID_ABI=armeabi
+# mkdir -p ../build/$ANDROID_ABI
+# export PATH=$ORIG_PATH:$NDK_DIR/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin
+# make all TARGET_ARCH_ABI=$ANDROID_ABI
+# if [ $? -eq 0 ]; then
+#     cp -r ../include/NTL ../build/$ANDROID_ABI
+#     cp ntl.a ../build/$ANDROID_ABI/libntl.a
+# fi
+
+# ANDROID_ABI=armeabi-v7a
+# mkdir -p ../build/$ANDROID_ABI
+# export PATH=$ORIG_PATH:$NDK_DIR/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin
+# make all TARGET_ARCH_ABI=$ANDROID_ABI
+# if [ $? -eq 0 ]; then
+#     cp -r ../include/NTL ../build/$ANDROID_ABI
+#     cp ntl.a ../build/$ANDROID_ABI/libntl.a
+# fi
+
+ANDROID_ABI=arm64-v8a
+mkdir -p ../build/$ANDROID_ABI
+export PATH=$ORIG_PATH:$NDK_DIR/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin
+make all TARGET_ARCH_ABI=$ANDROID_ABI
+if [ $? -eq 0 ]; then
+    cp -r ../include/NTL ../build/$ANDROID_ABI
+    cp ntl.a ../build/$ANDROID_ABI/libntl.a
+fi
+
 # cppkonsultants: compilation doesn't work for make check currently,
 # commented out
 # && make check
